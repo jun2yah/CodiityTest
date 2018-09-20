0ae4dba30acd17e60fe9bf097e8a7203b37a87a6
junhwan <jun2yah@gmail.com>
2018-09-20 15:18:28 +0900

    Commit

--- app/src/main/java/com/jun2yah/test/MainActivity.java
+++ app/src/main/java/com/jun2yah/test/MainActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 
 import com.jun2yah.test.codiitytest.R;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "Solution";
 
@@ -17,8 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
         Solution solution = new Solution();
 
-        int result = solution.solution1(561892);
-        Log.d(TAG, "solution1 = " + result);
+        int result1 = solution.solution1(561892);
+        Log.d(TAG, "result1 = " + result1);
+
+        int[] result2_1 = solution.solution2_1(new int[]{3, 8, 9, 7, 6}, 3);
+        Log.d(TAG, "result2_1 = " + Arrays.toString(result2_1));
+
+        int result2_2 = solution.solution2_2(new int[]{3, 8, 9, 7, 6});
+        Log.d(TAG, "result2_2 = " + result2_2);
+
+        int result3_3 = solution.solution3_3(new int[]{3, 1, 2, 4, 3});
+        Log.d(TAG, "result3_3 = " + result3_3);
 
     }
 }
--- app/src/main/java/com/jun2yah/test/Solution.java
+++ app/src/main/java/com/jun2yah/test/Solution.java
@@ -2,9 +2,19 @@ package com.jun2yah.test;
 
 import android.util.Log;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Solution {
     private final String TAG = "Solution";
 
+    /**
+     * BinaryGap
+     *
+     * @param N
+     * @return
+     */
     public int solution1(int N) {
         // write your code in Java SE 8
         String binary = Integer.toBinaryString(N);
@@ -26,12 +36,120 @@ public class Solution {
         }
 
         Log.d(TAG, binary);
-        Log.d(TAG, "" + max);
 
         return max;
     }
 
-//    public int[] solution2(int[] A, int K) {
-//        // write your code in Java SE 8
-//    }
+    /**
+     * CyclicRotation
+     *
+     * @param A
+     * @param K
+     * @return
+     */
+    public int[] solution2_1(int[] A, int K) {
+        // write your code in Java SE 8
+        int[] a = new int[A.length];
+
+        for (int i = 0; i < a.length; i++) {
+            a[(i+K)%a.length] = A[i];
+        }
+
+        return a;
+    }
+
+    /**
+     * OddOccurrencesInArray
+     *
+     * @param A
+     * @return
+     */
+    public int solution2_2(int[] A) {
+        // write your code in Java SE 8
+        Set<Integer> set = new HashSet<>();
+
+        for (int i: A) {
+            if (set.contains(i)) {
+                set.remove(i);
+            } else {
+                set.add(i);
+            }
+        }
+
+        return set.iterator().next();
+    }
+
+
+
+    /**
+     * PermMissingElem
+     *
+     * @param A
+     * @return
+     */
+    public int solution3_1(int[] A) {
+        // write your code in Java SE 8
+        long N = A.length + 1;
+        long total = N * (N + 1) / 2;
+
+        for (int i: A) {
+            total -= i;
+        }
+
+        return (int) total;
+    }
+
+    /**
+     * FrogJmp
+     *
+     * @param X
+     * @param Y
+     * @param D
+     * @return
+     */
+    public int solution3_2(int X, int Y, int D) {
+        // write your code in Java SE 8
+        int distance = Y - X;
+
+        return distance%D == 0 ? distance/D : distance/D + 1;
+    }
+
+    /**
+     * TapeEquilibrium
+     *
+     * @param A
+     * @return
+     */
+    public int solution3_3(int[] A) {
+        // write your code in Java SE 8
+        int minDiff = 0;
+
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                num1 += A[j];
+            }
+            for (int k = i + 1; k < A.length; k++) {
+                num2 += A[k];
+            }
+
+            int diff = Math.abs(num1 - num2);
+
+            if (i == 0) {
+                minDiff = diff;
+            } else {
+                if (minDiff > diff) {
+                    minDiff = diff;
+                }
+            }
+
+            num1 = 0;
+            num2 = 0;
+        }
+
+        return minDiff;
+    }
+
 }
